b798cb264d0afa76a36ce95deff3fef623716f8e
phoenix850508 <tom850508@gmail.com>
2023-10-30 00:07:58 +0800

    feat: user is able to upload profile image and username

--- src/components/myAccount/MyAccount.js
+++ src/components/myAccount/MyAccount.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import setting from "icons/undraw_personal_settings_kihd.svg";
 import NavbarContainer from "components/container/NavbarContainer";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { getAuth, updateProfile, signOut } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { db, app } from "utils/firebase";
 import Button from "components/buttons/Button";
 
@@ -10,35 +11,50 @@ function MyAccount() {
   let user = JSON.parse(localStorage.getItem("user"));
   const [accountNumber, setAccountNumber] = useState(null);
   const auth = getAuth(app);
-
   const [username, setUsername] = useState(user.displayName);
   const [previewAvatar, setPreviewAvatar] = useState(
     user.providerData[0].photoURL,
   );
+  const [avatar, setAvatar] = useState(null);
 
   // 儲存上傳的檔案
   const handleAvatarFile = (e) => {
     setPreviewAvatar(URL.createObjectURL(e.target.files[0]));
+    setAvatar(e.target.files[0]);
   };
 
   const handleSaveClick = async () => {
-    // 更新forestore資料
-    await updateDoc(doc(db, "users", user.uid), {
-      name: username,
-      photoURL: previewAvatar,
-    });
+    // 若尚未選取檔案，不動作
+    if (!avatar) return;
+    const storage = getStorage(app);
+    const metadata = {
+      contentType: avatar.type,
+    };
 
-    // 更新Auth中profile資料
-    await updateProfile(auth.currentUser, {
-      displayName: username,
-      photoURL: previewAvatar,
-    });
+    // 指定圖片上傳路徑
+    const storageRef = ref(storage, `images/${user.uid}`);
+    // 上傳圖片到FireStorage
+    uploadBytes(storageRef, avatar, metadata).then(() => {
+      getDownloadURL(storageRef).then(async (imageUrl) => {
+        // 更新forestore資料
+        await updateDoc(doc(db, "users", user.uid), {
+          name: username,
+          photoURL: imageUrl,
+        });
 
-    // 更新localStorage資料
-    user.displayName = username;
-    user.photoURL = previewAvatar;
-    localStorage.setItem("user", JSON.stringify(user));
-    alert("profile saved");
+        // 更新Auth中profile資料
+        await updateProfile(auth.currentUser, {
+          displayName: username,
+          photoURL: imageUrl,
+        });
+
+        // 更新localStorage資料
+        user.displayName = username;
+        user.photoURL = imageUrl;
+        localStorage.setItem("user", JSON.stringify(user));
+        alert("profile saved");
+      });
+    });
   };
   // 從fireStore取得user
   useEffect(() => {
